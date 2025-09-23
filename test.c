96a785364379c872099bdd695b519ee8e21b67db
sachins-devloper <sachinuce22@gmail.com>
2025-09-23 23:04:54 +0530

    for loop added

--- test.c
+++ test.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     // printf("Hello");
 
     // for loop
-    for (int i = 0; i < 10; i++){
+    for (int i = 0; i < 10; i++)
+    {
         printf("%d", i);
     }
 
-    char name[20] = "Sachin"; 
+    char name[20] = "Sachin";
+
+    for (int i = 0; i < 10; i++){
+        printf("%d", i);
+    }
 
     return 0;
 }
